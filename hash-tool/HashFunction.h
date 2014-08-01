98458f74090b8b8fab706a0a4f6b222fb3f069bb
TheNoobCompany <contact@thenoobcompany.com>
2014-08-02 02:11:52 +0300

    Re-implements authors headers to SHA-3 hashing library.
    Partial revert of "Add GUI into main.cpp - sha3sum.cpp is now deprecated and for testing only."
    
    This reverts commit eb0d5b08417e2ca019e917a9cfa175167a3de0ca.

--- hash-tool/HashFunction.h
+++ hash-tool/HashFunction.h
@@ -1,5 +1,14 @@
 #ifndef HASH_FUNCTION_H
-#define HASH_FUNCTION_
+#define HASH_FUNCTION_H
+
+/// Specification for a generic hash function to which our implemtation must
+/// conform. Adapted from HashFunction.java provided by Professor Alan Kaminsky
+/// at: http://www.cs.rit.edu/~ark/482/ugradproject/java2html.php?file=3
+///
+/// @author: Christopher Bentivenga
+/// @author: Frederick Christie
+/// @author: Michael Kitson
+/// @contributor: Alan Kaminsky
 
 class HashFunction{
  public:
