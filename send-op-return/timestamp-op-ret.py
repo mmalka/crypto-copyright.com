512e41c6da0d31a06a150a638e099a48f6bcade1
TheNoobCompany <contact@thenoobcompany.com>
2014-08-02 23:48:57 +0300

    Change timestamp-op-ret.py way of working.
    * Now only accept valid SHA-3/224 (28bytes) as inputs.
    * Create as much transactions as the amount of valid inputs referenced.
    * One invalid inputs break the whole generation and wont create any transaction.
    Note: This file have been CryptoProof hashed as copyrighted proof of existence.
    BTC TxId: 7550cf37fb758cb58ec282d222783bb7fd23142387ec4c207d92c977daaaf5eb
    SHA-3/224 of the file: 4b4dada08cb7280f092e22ac04a7b509cdf05922ae23b7861f561511
    OP_RETURN output: 43727970746f50726f6f662d4b4dada08cb7280f092e22ac04a7b509cdf05922ae23b7861f561511
    It have been submitted by a "prod" version of it. (0.001 BTC Fee and CryptoProof- marker), it's the first official CryptoProof- registred file :)

--- send-op-return/timestamp-op-ret.py
+++ send-op-return/timestamp-op-ret.py
@@ -8,29 +8,58 @@
 import hashlib
 import bitcoin.rpc
 import sys
+import string
 
 from bitcoin.core import *
 from bitcoin.core.script import *
 
+def is_hex(s):
+     hex_digits = set(string.hexdigits)
+     # if s is long, then it is faster to check against a set
+     return all(c in hex_digits for c in s)
 proxy = bitcoin.rpc.Proxy()
-
+print("-----------------------------+=======================================================================+---------------------------")
+print("-----------------------------+ OP_RETURN transaction builder by crypto-copyright.com, copyright 2014 +---------------------------");
+print("-----------------------------+  Builds raw transaction, signs it, broadcasts it to bitcoin network.  +---------------------------")
+print("-----------------------------+             usage: script.py [\"valid-sha3/224-hash1\",...]             +---------------------------");
+print("-----------------------------+=======================================================================+---------------------------")
 assert len(sys.argv) > 1
 
+FEE_TO_PAY_PER_BYTE = 0.0001; # prod: 0.001
+byte_marker = "CryptoTests-"; # prod: "CryptoProof-";
 digests = []
-for f in sys.argv[1:]:
+for myHash in sys.argv[1:]:
     try:
-        with open(f, 'rb') as fd:
-            digests.append(Hash(fd.read()))
-    except FileNotFoundError as exp:
-        if len(f)/2 in (20, 32):
-            digests.append(x(f))
-        else:
-            raise exp
+        if len(byte_marker) > 12:
+            print("The 'byte_marker' variable is not valid, should contains at most 12 characters, currently containing " + str(len(byte_marker)))
+            exit(0)
+        if not is_hex(myHash):
+            print("The input is not a valid hex hash.")
+            print("Size: " + str(len(myHash)) + ", input: " + str(myHash))
+            exit(0)
+        if len(myHash) < 56:
+            print("The input hash is not a valid SHA-3/224 hash.")
+            print("Size: " + str(len(myHash)) + ", input: " + str(myHash))
+            exit(0)
+        if len(myHash) > 56:
+            print("The input hash is not a valid SHA-3/224 hash.")
+            print("Size: " + str(len(myHash)) + ", input: " + str(myHash))
+            exit(0)
+        print("The input hash is a valid SHA-3/224 hash | Size: " + str(len(myHash)) + " | input: " + str(myHash))
+        output_hash = (binascii.hexlify(byte_marker.encode('ascii')) + myHash.encode('ascii'));
+# We have a valid input, let's append our marker to it.
+        digests.append(output_hash)
     except IOError as exp:
         print(exp,file=sys.stderr)
         continue
-
+print("---------------------------------------------------------------------------------------------------------------------------------")
+print("Processing the inputs... " + str(len(digests)) + " valids SHA-3/224 hashs found.")
+print("---------------------------------------------------------------------------------------------------------------------------------")
+i = 1;
 for digest in digests:
+    print("Processing #" + str(i) + " OP_RETURN | Size: " + str(len(digest)) + " | Value: " + str(digest))
+    print("---------------------------------------------------------------------------------------------------------------------------------")
+    i += 1
     txouts = []
 
     unspent = sorted(proxy.listunspent(0), key=lambda x: hash(x['amount']))
@@ -40,25 +69,26 @@ for digest in digests:
 
     change_addr = proxy.getnewaddress()
     change_pubkey = proxy.validateaddress(change_addr)['pubkey']
-    change_out = CMutableTxOut(MAX_MONEY, CScript([change_pubkey, OP_CHECKSIG]))
+    digest_outs = CMutableTxOut(0, CScript([script.OP_RETURN, x(digest.decode('UTF-8'))]))
 
-    digest_outs = [CMutableTxOut(0, CScript([script.OP_RETURN, digest]))]
-
-    txouts = [change_out] + digest_outs
+    
+    change_out = [CMutableTxOut(MAX_MONEY, CScript([change_pubkey, OP_CHECKSIG]))]
+    txouts = [digest_outs] + change_out
 
     tx = CMutableTransaction(txins, txouts)
 
-
     FEE_PER_BYTE = 0.00025*COIN/1000
     while True:
-        tx.vout[0].nValue = int(value_in - max(len(tx.serialize())*FEE_PER_BYTE, 0.00011*COIN))
+        tx.vout[1].nValue = int(value_in - max(len(tx.serialize())*FEE_PER_BYTE, FEE_TO_PAY_PER_BYTE*COIN))
 
         r = proxy.signrawtransaction(tx)
         assert r['complete']
         tx = r['tx']
 
-        if value_in - tx.vout[0].nValue >= len(tx.serialize())*FEE_PER_BYTE:
-            print(b2x(tx.serialize()))
-            print(len(tx.serialize()),'bytes',file=sys.stderr)
-            print(b2lx(proxy.sendrawtransaction(tx)))
+        if value_in - tx.vout[1].nValue >= len(tx.serialize())*FEE_PER_BYTE:
+            content_txraw = b2x(tx.serialize());
+            size_txraw = str(len(tx.serialize())) + " bytes";
+            print("Generated TxRaw | Size: " + str(size_txraw) + " | Value: " + content_txraw)
+            print("Transaction Broadcast -> Generated TxId | " + b2lx(proxy.sendrawtransaction(tx)))
+            print("---------------------------------------------------------------------------------------------------------------------------------")
             break
