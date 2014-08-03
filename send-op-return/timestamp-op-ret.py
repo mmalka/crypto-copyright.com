b0f726eacd33303644670395d29d9e00fc1bac85
TheNoobCompany <contact@thenoobcompany.com>
2014-08-03 07:01:22 +0300

    Add a parameter "-s" / "--silent-mode" in our OP_RETURN transaction builder. It will only output the transaction id if used.

--- send-op-return/timestamp-op-ret.py
+++ send-op-return/timestamp-op-ret.py
@@ -18,17 +18,22 @@ def is_hex(s):
      # if s is long, then it is faster to check against a set
      return all(c in hex_digits for c in s)
 proxy = bitcoin.rpc.Proxy()
-print("-----------------------------+=======================================================================+---------------------------")
-print("-----------------------------+ OP_RETURN transaction builder by crypto-copyright.com, copyright 2014 +---------------------------");
-print("-----------------------------+  Builds raw transaction, signs it, broadcasts it to bitcoin network.  +---------------------------")
-print("-----------------------------+             usage: script.py [\"valid-sha3/224-hash1\",...]             +---------------------------");
-print("-----------------------------+=======================================================================+---------------------------")
+silent_mode = 0
+stdrin = sys.argv[1:]
+if sys.argv[1] == "-s" or sys.argv[1] == "--silent-mode":
+    silent_mode = 1
+    stdrin = sys.argv[2:]
+if silent_mode == 0: print("-----------------------------+=======================================================================+---------------------------")
+if silent_mode == 0: print("-----------------------------+ OP_RETURN transaction builder by crypto-copyright.com, copyright 2014 +---------------------------")
+if silent_mode == 0: print("-----------------------------+  Builds raw transaction, signs it, broadcasts it to bitcoin network.  +---------------------------")
+if silent_mode == 0: print("-----------------------------+             usage: script.py [\"valid-sha3/224-hash1\",...]             +---------------------------")
+if silent_mode == 0: print("-----------------------------+=======================================================================+---------------------------")
 assert len(sys.argv) > 1
 
 FEE_TO_PAY_PER_BYTE = 0.0001; # prod: 0.001
 byte_marker = "CryptoTests-"; # prod: "CryptoProof-";
 digests = []
-for myHash in sys.argv[1:]:
+for myHash in stdrin:
     try:
         if len(byte_marker) > 12:
             print("The 'byte_marker' variable is not valid, should contains at most 12 characters, currently containing " + str(len(byte_marker)))
@@ -45,20 +50,20 @@ for myHash in sys.argv[1:]:
             print("The input hash is not a valid SHA-3/224 hash.")
             print("Size: " + str(len(myHash)) + ", input: " + str(myHash))
             exit(0)
-        print("The input hash is a valid SHA-3/224 hash | Size: " + str(len(myHash)) + " | input: " + str(myHash))
+        if silent_mode == 0: print("The input hash is a valid SHA-3/224 hash | Size: " + str(len(myHash)) + " | input: " + str(myHash))
         output_hash = (binascii.hexlify(byte_marker.encode('ascii')) + myHash.encode('ascii'));
 # We have a valid input, let's append our marker to it.
         digests.append(output_hash)
     except IOError as exp:
         print(exp,file=sys.stderr)
         continue
-print("---------------------------------------------------------------------------------------------------------------------------------")
-print("Processing the inputs... " + str(len(digests)) + " valids SHA-3/224 hashs found.")
-print("---------------------------------------------------------------------------------------------------------------------------------")
+if silent_mode == 0: print("---------------------------------------------------------------------------------------------------------------------------------")
+if silent_mode == 0: print("Processing the inputs... " + str(len(digests)) + " valids SHA-3/224 hashs found.")
+if silent_mode == 0: print("---------------------------------------------------------------------------------------------------------------------------------")
 i = 1;
 for digest in digests:
-    print("Processing #" + str(i) + " OP_RETURN | Size: " + str(len(digest)) + " | Value: " + str(digest))
-    print("---------------------------------------------------------------------------------------------------------------------------------")
+    if silent_mode == 0: print("Processing #" + str(i) + " OP_RETURN | Size: " + str(len(digest)) + " | Value: " + str(digest))
+    if silent_mode == 0: print("---------------------------------------------------------------------------------------------------------------------------------")
     i += 1
     txouts = []
 
@@ -88,7 +93,9 @@ for digest in digests:
         if value_in - tx.vout[1].nValue >= len(tx.serialize())*FEE_PER_BYTE:
             content_txraw = b2x(tx.serialize());
             size_txraw = str(len(tx.serialize())) + " bytes";
-            print("Generated TxRaw | Size: " + str(size_txraw) + " | Value: " + content_txraw)
-            print("Transaction Broadcast -> Generated TxId | " + b2lx(proxy.sendrawtransaction(tx)))
-            print("---------------------------------------------------------------------------------------------------------------------------------")
+            generated_tx = b2lx(proxy.sendrawtransaction(tx));
+            if silent_mode == 0: print("Generated TxRaw | Size: " + str(size_txraw) + " | Value: " + content_txraw)
+            if silent_mode == 0: print("Transaction Broadcast -> Generated TxId | " + generated_tx)
+            if silent_mode == 0: print("---------------------------------------------------------------------------------------------------------------------------------")
+            if silent_mode == 1: print(generated_tx)
             break
